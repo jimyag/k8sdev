55c80f97bc745a7f967b8b877a3c3561a6cae543
jimyag <jiaminyag@gmail.com>
2024-08-11 14:40:56 +0800

    feat: can filter custom label

--- Makefile
+++ Makefile
@@ -8,13 +8,30 @@ baseImageTag?=$(shell TZ='Asia/Shanghai' date "+%Y-%m-%d-%H-%M-%S")
 
 .PHONY: build tidy container-scheduler
 
+
+
 build: tidy
-	CGO_ENABLED=0 GOBIN=$(GOBIN) go install -trimpath -v ./...
+	CGO_ENABLED=0 go install -trimpath -v ./...
+
+docker-base:
+	@if [ "$(UNAME_S)" = "Linux" ] && [ "$(UNAME_M)" = "x86_64" ]; then \
+		echo "Running target for AMD64 Linux"; \
+		$(MAKE) build; \
+	else \
+		echo "Running target for non-AMD64 or non-Linux"; \
+		$(MAKE) amd64_linux; \
+	fi
+
+amd64_linux:
+	GOMODCACHE=$(GOPATH)/pkg/mod CGO_ENABLED=0 GOPATH=$(GOBIN) GOARCH=amd64 GOOS=linux go install -trimpath -v  ./...
+	mkdir -p $(GOBIN)/linux_amd64
+	mv -f $(GOBIN)/bin/linux_amd64/* $(GOBIN)/
+	rm -rf $(GOBIN)/bin/ $(GOBIN)/linux_amd64
 
 tidy:
 	go mod tidy
 
-container-scheduler: build
+container-scheduler: docker-base
 	docker build -t $(baseImageUrl)/k8sdev/scheduler:$(baseImageTag) \
 		--progress=plain --target scheduler --push --platform linux/amd64 \
 		-f ./Dockerfile  .
--- scheduler/internal/custom.go
+++ scheduler/internal/custom.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"context"
-	"time"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -65,21 +64,21 @@ const (
 	// 插件名称
 	Name = "JimyagCustom"
 	// 插件状态存储的 key
-	StateKye = Name + "State"
+	FilterLabel = "filter.k8sdev.jimyag.com"
 )
 
 var (
-	_ framework.Plugin           = &JimyagCustom{}
-	_ framework.PreFilterPlugin  = &JimyagCustom{}
-	_ framework.FilterPlugin     = &JimyagCustom{}
-	_ framework.PostFilterPlugin = &JimyagCustom{}
-	_ framework.PreScorePlugin   = &JimyagCustom{}
-	_ framework.ScorePlugin      = &JimyagCustom{} // 包含 NormalizeScore
-	_ framework.ReservePlugin    = &JimyagCustom{}
-	_ framework.PermitPlugin     = &JimyagCustom{}
-	_ framework.PreBindPlugin    = &JimyagCustom{}
-	_ framework.BindPlugin       = &JimyagCustom{}
-	_ framework.PostBindPlugin   = &JimyagCustom{}
+	_ framework.Plugin = &JimyagCustom{}
+	// _ framework.PreFilterPlugin = &JimyagCustom{}
+	_ framework.FilterPlugin = &JimyagCustom{}
+	// _ framework.PostFilterPlugin = &JimyagCustom{}
+	// _ framework.PreScorePlugin   = &JimyagCustom{}
+	// _ framework.ScorePlugin      = &JimyagCustom{} // 包含 NormalizeScore
+	//_ framework.ReservePlugin  = &JimyagCustom{}
+	// _ framework.PermitPlugin   = &JimyagCustom{}
+	// _ framework.PreBindPlugin  = &JimyagCustom{}
+	// _ framework.BindPlugin     = &JimyagCustom{}
+	// _ framework.PostBindPlugin = &JimyagCustom{}
 )
 
 type StateData struct {
@@ -94,13 +93,17 @@ func (s *StateData) Clone() framework.StateData {
 func New(rawArgs runtime.Object, h framework.Handle) (framework.Plugin, error) {
 	klog.Infof("New Scheduling plugin: %s", Name)
 	// 拿到 kube config
-	c := JimyagCustom{}
+	c := JimyagCustom{
+		handle: h,
+	}
 	klog.Infof("%s plugin initialized", Name)
 	return &c, nil
 }
 
 // JimyagCustom 是一个实现了所有扩展点 的示例插件
-type JimyagCustom struct{}
+type JimyagCustom struct {
+	handle framework.Handle
+}
 
 // Name 返回插件名称
 func (c *JimyagCustom) Name() string {
@@ -109,123 +112,144 @@ func (c *JimyagCustom) Name() string {
 
 // PreFilter pod 预处理和检查，不符合预期就提前结束调度
 // state 可用于保存一些状态信息，然后在后面的扩展点（例如 Filter() 阶段）拿出来用
-func (c *JimyagCustom) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod) (*framework.PreFilterResult, *framework.Status) {
-	klog.Infof("PreFilter %s/%s: start", pod.Namespace, pod.Name)
-	// 做一些检查
-	state.Write(StateKye, &StateData{
-		Msg: "PreFilter set state",
-	})
-	klog.Infof("PreFilter %s/%s: finish", pod.Namespace, pod.Name)
-	return nil, framework.NewStatus(framework.Success, "")
-}
+// func (c *JimyagCustom) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod) (*framework.PreFilterResult, *framework.Status) {
+// 	klog.Infof("PreFilter %s/%s: start", pod.Namespace, pod.Name)
+// 	// 做一些检查
+// 	state.Write(StateKye, &StateData{
+// 		Msg: "PreFilter set state",
+// 	})
+// 	klog.Infof("PreFilter %s/%s: finish", pod.Namespace, pod.Name)
+// 	return nil, framework.NewStatus(framework.Success, "")
+// }
 
 // PreFilterExtensions returns preFilter extensions, pod add and remove.
-func (c *JimyagCustom) PreFilterExtensions() framework.PreFilterExtensions {
-	return c
-}
+// func (c *JimyagCustom) PreFilterExtensions() framework.PreFilterExtensions {
+// 	return c
+// }
 
 // AddPod from pre-computed data in cycleState.
 // no current need for this method.
-func (c *JimyagCustom) AddPod(ctx context.Context, cycleState *framework.CycleState, podToSchedule *v1.Pod, podToAdd *framework.PodInfo, nodeInfo *framework.NodeInfo) *framework.Status {
-	klog.Infof("AddPod %s/%s: start", podToSchedule.Namespace, podToSchedule.Name)
-	klog.Infof("AddPod %s/%s: nodeName: %s", podToSchedule.Namespace, podToSchedule.Name, nodeInfo.Node().Name)
-	klog.Infof("AddPod %s/%s: finish", podToSchedule.Namespace, podToSchedule.Name)
-	return framework.NewStatus(framework.Success, "")
-}
+// func (c *JimyagCustom) AddPod(ctx context.Context, cycleState *framework.CycleState, podToSchedule *v1.Pod, podToAdd *framework.PodInfo, nodeInfo *framework.NodeInfo) *framework.Status {
+// 	klog.Infof("AddPod %s/%s: start", podToSchedule.Namespace, podToSchedule.Name)
+// 	klog.Infof("AddPod %s/%s: nodeName: %s", podToSchedule.Namespace, podToSchedule.Name, nodeInfo.Node().Name)
+// 	klog.Infof("AddPod %s/%s: finish", podToSchedule.Namespace, podToSchedule.Name)
+// 	return framework.NewStatus(framework.Success, "")
+// }
 
 // RemovePod from pre-computed data in cycleState.
 // no current need for this method.
-func (c *JimyagCustom) RemovePod(ctx context.Context, cycleState *framework.CycleState, podToSchedule *v1.Pod, podToRemove *framework.PodInfo, nodeInfo *framework.NodeInfo) *framework.Status {
-	klog.Infof("RemovePod %s/%s: start", podToSchedule.Namespace, podToSchedule.Name)
-	klog.Infof("RemovePod %s/%s: nodeName: %s", podToSchedule.Namespace, podToSchedule.Name, nodeInfo.Node().Name)
-	klog.Infof("RemovePod %s/%s: finish", podToSchedule.Namespace, podToSchedule.Name)
-	return framework.NewStatus(framework.Success, "")
-}
+// func (c *JimyagCustom) RemovePod(ctx context.Context, cycleState *framework.CycleState, podToSchedule *v1.Pod, podToRemove *framework.PodInfo, nodeInfo *framework.NodeInfo) *framework.Status {
+// 	klog.Infof("RemovePod %s/%s: start", podToSchedule.Namespace, podToSchedule.Name)
+// 	klog.Infof("RemovePod %s/%s: nodeName: %s", podToSchedule.Namespace, podToSchedule.Name, nodeInfo.Node().Name)
+// 	klog.Infof("RemovePod %s/%s: finish", podToSchedule.Namespace, podToSchedule.Name)
+// 	return framework.NewStatus(framework.Success, "")
+// }
 
 // Filter 过滤掉那些不满足要求的 node
+// 过滤掉不包含
 func (c *JimyagCustom) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
-	klog.Infof("Filter %s/%s: start", pod.Namespace, pod.Name)
-	klog.Infof("Filter %s/%s: nodeName: %s", pod.Namespace, pod.Name, nodeInfo.Node().Name)
-	data, err := state.Read(StateKye)
-	// 如果没有拿到 state，直接返回
-	if err != nil {
-		return framework.NewStatus(framework.Error, err.Error())
+	node := nodeInfo.Node()
+	klog.Infof("Filter %s/%s/%s: start scheduler plugin", pod.Namespace, pod.Name, node.Name)
+	podValue, found := pod.GetLabels()[FilterLabel]
+	if !found {
+		// 如果没有 pod 不包含  自定义的 filter，则可以任意分配
+		klog.Infof("Filter %s/%s/%s: pod doesn't have '%s' label; can be scheduled",
+			pod.Namespace, pod.Name, node.Name,
+			FilterLabel)
+		return framework.NewStatus(framework.Success, "")
 	}
-	klog.Infof("Filter %s/%s: stateData: %s", pod.Namespace, pod.Name, data.(*StateData).Msg)
-	klog.Infof("Filter %s/%s: finish", pod.Namespace, pod.Name)
+	nodeValue, found := node.GetLabels()[FilterLabel]
+	if !found {
+		// 如果 node 没有 自定义的 label，则不能被分配
+		klog.Infof("Filter %s/%s/%s:  node doesn't have '%s' label; pod cannot be scheduled here",
+			pod.Namespace, pod.Name, node.Name,
+			FilterLabel)
+		return framework.NewStatus(framework.Unschedulable, "node does not have required label")
+	}
+	if nodeValue != podValue {
+		// 如果 pod 中 filter label 的值不等于 node 中 filter label 的值，则不能被分配
+		klog.Infof("Filter %s/%s/%s: node '%s' label value %s does not match pod label value (%s)",
+			pod.Namespace, pod.Name, node.Name,
+			FilterLabel, nodeValue, podValue)
+		return framework.NewStatus(framework.Unschedulable, "nodeGroup of pod and node don't match")
+	}
+
+	klog.Infof("Filter %s/%s/%s: finished, node '%s' : values match %s",
+		pod.Namespace, pod.Name, node.Name,
+		FilterLabel, nodeValue)
 	// 拿到 state，进行下一步操作
 	return framework.NewStatus(framework.Success, "")
 }
 
 // PostFilter 如果 Filter 阶段之后，所有 nodes 都被筛掉了，一个都没剩，才会执行这个阶段；否则不会执行这个阶段的 plugins。
 // 按 plugin 顺序依次执行，任何一个插件将 node 标记为 Shedable 就算成功，不再执行剩下的 PostFilter plugins。
-func (c *JimyagCustom) PostFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, filteredNodeStatusMap framework.NodeToStatusMap) (*framework.PostFilterResult, *framework.Status) {
-	klog.Infof("PostFilter %s/%s: start", pod.Namespace, pod.Name)
-	klog.Infof("PostFilter %s/%s: finish", pod.Namespace, pod.Name)
-	return nil, framework.NewStatus(framework.Success, "")
-}
+// func (c *JimyagCustom) PostFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, filteredNodeStatusMap framework.NodeToStatusMap) (*framework.PostFilterResult, *framework.Status) {
+// 	klog.Infof("PostFilter %s/%s: start", pod.Namespace, pod.Name)
+// 	klog.Infof("PostFilter %s/%s: finish", pod.Namespace, pod.Name)
+// 	return nil, framework.NewStatus(framework.Success, "")
+// }
 
-func (c *JimyagCustom) PreScore(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodes []*v1.Node) *framework.Status {
-	klog.Infof("PreScore %s/%s: start", pod.Namespace, pod.Name)
-	klog.Infof("PreScore %s/%s: finish", pod.Namespace, pod.Name)
-	return framework.NewStatus(framework.Success, "")
-}
+// func (c *JimyagCustom) PreScore(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodes []*v1.Node) *framework.Status {
+// 	klog.Infof("PreScore %s/%s: start", pod.Namespace, pod.Name)
+// 	klog.Infof("PreScore %s/%s: finish", pod.Namespace, pod.Name)
+// 	return framework.NewStatus(framework.Success, "")
+// }
 
-func (c *JimyagCustom) Score(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) (int64, *framework.Status) {
-	klog.Infof("Score %s/%s: start", p.Namespace, p.Name)
-	klog.Infof("Score %s/%s: finish", p.Namespace, p.Name)
-	return 0, framework.NewStatus(framework.Success, "")
-}
+// func (c *JimyagCustom) Score(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) (int64, *framework.Status) {
+// 	klog.Infof("Score %s/%s: start", p.Namespace, p.Name)
+// 	klog.Infof("Score %s/%s: finish", p.Namespace, p.Name)
+// 	return 0, framework.NewStatus(framework.Success, "")
+// }
 
-func (c *JimyagCustom) ScoreExtensions() framework.ScoreExtensions {
-	return c
-}
+// func (c *JimyagCustom) ScoreExtensions() framework.ScoreExtensions {
+// 	return c
+// }
 
-// NormalizeScore implements framework.ScoreExtensions.
-func (c *JimyagCustom) NormalizeScore(ctx context.Context, state *framework.CycleState, p *v1.Pod, scores framework.NodeScoreList) *framework.Status {
-	klog.Infof("NormalizeScore %s/%s: start", p.Namespace, p.Name)
-	klog.Infof("NormalizeScore %s/%s: finish", p.Namespace, p.Name)
-	return framework.NewStatus(framework.Success, "")
-}
+// // NormalizeScore implements framework.ScoreExtensions.
+// func (c *JimyagCustom) NormalizeScore(ctx context.Context, state *framework.CycleState, p *v1.Pod, scores framework.NodeScoreList) *framework.Status {
+// 	klog.Infof("NormalizeScore %s/%s: start", p.Namespace, p.Name)
+// 	klog.Infof("NormalizeScore %s/%s: finish", p.Namespace, p.Name)
+// 	return framework.NewStatus(framework.Success, "")
+// }
 
-func (c *JimyagCustom) Reserve(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
-	klog.Infof("Reserve %s/%s: start", p.Namespace, p.Name)
-	klog.Infof("Reserve %s/%s: finish", p.Namespace, p.Name)
-	return framework.NewStatus(framework.Success, "")
-}
+// func (c *JimyagCustom) Reserve(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
+// 	klog.Infof("Reserve %s/%s: start", p.Namespace, p.Name)
+// 	klog.Infof("Reserve %s/%s: finish", p.Namespace, p.Name)
+// 	return framework.NewStatus(framework.Success, "")
+// }
 
-func (c *JimyagCustom) Unreserve(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) {
-	klog.Infof("Unreserve %s/%s: start", p.Namespace, p.Name)
-	klog.Infof("Unreserve %s/%s: finish", p.Namespace, p.Name)
-}
+// func (c *JimyagCustom) Unreserve(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) {
+// 	klog.Infof("Unreserve %s/%s: start", p.Namespace, p.Name)
+// 	klog.Infof("Unreserve %s/%s: finish", p.Namespace, p.Name)
+// }
 
 // Permit approve：所有 Permit plugins 都 appove 之后，这个 pod 就进入下面的 binding 阶段；
 // deny：任何一个 Permit plugin deny 之后，就无法进入 binding 阶段。这会触发 Reserve plugins 的 Unreserve() 方法；
 // wait (with a timeout)：如果有 Permit plugin 返回“wait”，这个 pod 就会进入一个 internal“waiting”Pods list；
-func (c *JimyagCustom) Permit(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) (*framework.Status, time.Duration) {
-	klog.Infof("Permit %s/%s: start", p.Namespace, p.Name)
-	klog.Infof("Permit %s/%s: finish", p.Namespace, p.Name)
-	return framework.NewStatus(framework.Success, ""), 0
-}
+// func (c *JimyagCustom) Permit(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) (*framework.Status, time.Duration) {
+// 	klog.Infof("Permit %s/%s: start", p.Namespace, p.Name)
+// 	klog.Infof("Permit %s/%s: finish", p.Namespace, p.Name)
+// 	return framework.NewStatus(framework.Success, ""), 0
+// }
 
 // PreBind Bind 之前的预处理，例如到 node 上去挂载 volume
-func (c *JimyagCustom) PreBind(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
-	klog.Infof("PreBind %s/%s: start", p.Namespace, p.Name)
-	klog.Infof("PreBind %s/%s: finish", p.Namespace, p.Name)
-	return framework.NewStatus(framework.Success, "")
-}
+// func (c *JimyagCustom) PreBind(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
+// 	klog.Infof("PreBind %s/%s: start", p.Namespace, p.Name)
+// 	klog.Infof("PreBind %s/%s: finish", p.Namespace, p.Name)
+// 	return framework.NewStatus(framework.Success, "")
+// }
 
 // Bind 绑定
-func (c *JimyagCustom) Bind(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
-	klog.Infof("Bind %s/%s: start", p.Namespace, p.Name)
-	klog.Infof("Bind %s/%s: finish", p.Namespace, p.Name)
-	return framework.NewStatus(framework.Success, "")
-}
+// func (c *JimyagCustom) Bind(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
+// 	klog.Infof("Bind %s/%s: start", p.Namespace, p.Name)
+// 	klog.Infof("Bind %s/%s: finish", p.Namespace, p.Name)
+// 	return framework.NewStatus(framework.Success, "")
+// }
 
 // PostBind 绑定之后的处理 这是一个 informational extension point，也就是无法影响调度决策（没有返回值）。
 // bind 成功的 pod 才会进入这个阶段；
 // 作为 binding cycle 的最后一个阶段，一般是用来清理一些相关资源。
-func (c *JimyagCustom) PostBind(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) {
-	klog.Infof("PostBind %s/%s: start", pod.Namespace, pod.Name)
-	klog.Infof("PostBind %s/%s: finish", pod.Namespace, pod.Name)
-}
+// func (c *JimyagCustom) PostBind(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) {
+// 	klog.Infof("PostBind %s/%s: start", pod.Namespace, pod.Name)
+// 	klog.Infof("PostBind %s/%s: finish", pod.Namespace, pod.Name)
+// }
